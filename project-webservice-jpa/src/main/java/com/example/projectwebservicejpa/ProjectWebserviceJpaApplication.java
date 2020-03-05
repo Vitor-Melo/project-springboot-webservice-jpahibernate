f2bd8786a7ec65b96a5ff8b5f1d897d888a6cdff
Vitor-Melo <vitorpmelo20@gmail.com>
2020-03-05 01:22:19 -0300

    Projeto criado

---
+++ project-webservice-jpa/src/main/java/com/example/projectwebservicejpa/ProjectWebserviceJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectwebservicejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWebserviceJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWebserviceJpaApplication.class, args);
+	}
+
+}
---
+++ project-webservice-jpa/src/test/java/com/example/projectwebservicejpa/ProjectWebserviceJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectwebservicejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWebserviceJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
